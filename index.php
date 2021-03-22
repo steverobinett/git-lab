48e1a57f65f8759c0ba33d6916efa19ede2f7e02
Steve Robinett <steven.robinett@gfcmsu.edu>
2021-03-22 11:56:52 -0600

    Create landing page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Demo</title>
+</head>
+<body>
+    <h1>Welcome to the git Demo Site</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, dolore explicabo eos porro quam ullam. Itaque molestiae quaerat voluptatem sint omnis obcaecati in veritatis esse consequatur rem mollitia, ea minima.</p>
+</body>
+</html>
