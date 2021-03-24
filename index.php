73921ed7c583172e9f4e3a2d6b36bff8264f1063
Steve Robinett <steven.robinett@gfcmsu.edu>
2021-03-24 12:43:30 -0600

    Added styles

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
     <title>Git Demo</title>
 </head>
 <body>
-    <h1>Welcome to the git Demo Site</h1>
+    <h1 id="heading">Welcome to the git Demo Site</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, dolore explicabo eos porro quam ullam. Itaque molestiae quaerat voluptatem sint omnis obcaecati in veritatis esse consequatur rem mollitia, ea minima.</p>
 
     <p>A bit more</p>
